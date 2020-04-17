fec57f0cea17ed022f6f34f0184b3ceb1ca8de06
Arnaud Duhé <anro010@hotmail.com>
2020-04-17 19:11:14 +0100

    header image fixed

--- src/components/home/header/header.js
+++ src/components/home/header/header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './header.css';
 
 const Header = () => (
-    <div className="container-home-welcome-section">
+  <div className="container-home-welcome-section">
     <h1>Get my recipe</h1>
     </div>
 );
