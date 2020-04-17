306042eff42ebe3e77bcaf63a68fa2d484656eca
Arnaud Duhé <anro010@hotmail.com>
2020-04-17 18:46:26 +0100

    added photos - css for header

---
+++ src/components/home/header/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => (
+    <div className="container-home-welcome-section">
+    <h1>Get my recipe</h1>
+    </div>
+);
+
+export default Header ;
