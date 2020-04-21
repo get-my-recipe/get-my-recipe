568a191cc001fb40aaa31c6e4feed2d487822cdc
Catherine <catblecon@gmail.com>
2020-04-21 20:53:09 +0100

    css-component Nav

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
 import './App.css';
+
 import Home from './components/home/home';
 import Contact from './components/contact/contact';
 import About from './components/about/about';
