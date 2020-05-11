6a3db924eff05e51b3548e2d375454909f029a46
Numissa11 <aline_sinclaire@hotmail.fr>
2020-05-11 22:21:18 +0100

    contact-us finished and about us picture loaded

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
 import Form from "react-bootstrap/Form";
-import NavBarShared from '../sharedcomponents/navbarshared/navbarshared';
-import './contact.css';
+import NavBarShared from "../sharedcomponents/navbarshared/navbarshared";
+import "./contact.css";
 
 class ContactForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: '',
+      message: "",
     };
   }
 
   onSubmit = (event) => {
     event.preventDefault();
-      alert(`${'Your message was sent successfully!'}`);
+    alert(`${"Your message was sent successfully!"}`);
   };
 
   handleChange = (event) => {
@@ -24,37 +24,43 @@ class ContactForm extends Component {
 
   render() {
     return (
-      <div>
-        <NavBarShared />
-        <form className="contactUsForm">
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Name</Form.Label>
-            <Form.Control style={{ fontFamily: 'helvetica' }} type="email" placeholder="Enter Name..." />
-          </Form.Group>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control style={{ fontFamily: 'helvetica' }} type="email" placeholder="Enter email..." />
-            <Form.Text className="text-muted">
-              We&apos;ll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
-          <h2>Tell us your inspiration of the day:</h2>
-          <textarea
-            style={{ fontFamily: 'helvetica' }}
-            placeholder="Write something here..."
-            id="message-id"
-            name="message"
-            type="text"
-            value={this.state.message}
-            onChange={(event) => this.handleChange(event)}
-            rows={8}
-          />
-          <p>
-            <button onClick={(event) => this.onSubmit(event)} type="submit">
-              Send
-            </button>
-          </p>
-        </form>
+      <div className="containerForm">
+        <div>
+          <NavBarShared />
+          <form className="contactUsForm">
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label className="label2">Name</Form.Label>
+              <Form.Control type="email" placeholder="Enter Name..." />
+            </Form.Group>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label className="label2">E-mail address</Form.Label>
+              <Form.Control
+                className="ligne"
+                type="email"
+                placeholder="Enter email..."
+              />
+              <Form.Text className="text-muted">
+                We&apos;ll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <h2>Tell us your inspiration of the day:</h2>
+            <textarea
+              style={{ paddingLeft:"11px" }}
+              placeholder="Write something here..."
+              id="message-id"
+              name="message"
+              type="text"
+              value={this.state.message}
+              onChange={(event) => this.handleChange(event)}
+              rows={8}
+            />
+            <p>
+              <button onClick={(event) => this.onSubmit(event)} type="submit">
+                Send
+              </button>
+            </p>
+          </form>
+        </div>
       </div>
     );
   }
