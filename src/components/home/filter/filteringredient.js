9c210615917f4987da0a61a84805b86c170cd7b4
Arnaud Duhé <anro010@hotmail.com>
2020-05-12 08:47:08 +0100

    style on its way

--- src/components/home/filter/filteringredient.js
+++ src/components/home/filter/filteringredient.js
@@ -1,36 +1,35 @@
-import React from "react";
+import React from 'react';
 import CircularSlider from '@fseehawer/react-circular-slider';
 import './circular.css';
 // import { ReactComponent as PowerIcon } from './assets/power.svg';
 
-const Filteringredient =({handleInputChangeIngr}) => (
+const Filteringredient = ({ handleInputChangeIngr }) => (
 
-  
-  <CircularSlider className='circuSlider' 
-  width={120}
-  heigh={120}
-  label="Ingredients"
-  min={1}
-  max={20}
-  dataIndex={20}
-  valueFontSize = '1.5rem'
+
+  <CircularSlider
+    className="circuSlider"
+    width={120}
+    heigh={120}
+    label="Ingredients"
+    min={1}
+    max={20}
+    dataIndex={20}
+    valueFontSize="1.5rem"
   // prependToValue="$"
   // appendToValue="K"
-  labelColor="#9EB25D"
-  labelBottom={true}
-  knobColor="#9EB25D"
-  progressColorFrom="#EDFF71"
-  progressColorTo="#9EB25D"
-  progressSize={7}
-  trackColor="#F1DB4B"
-  trackSize={7}
-  onChange={ (value) => {handleInputChangeIngr(value) } }
->
-  {/* <DragIcon x="9" y="8" width="18px" height="18px" /> */}
-</CircularSlider>
-    );
+    labelColor="#9EB25D"
+    labelBottom
+    knobColor="#9EB25D"
+    progressColorFrom="#EDFF71"
+    progressColorTo="#9EB25D"
+    progressSize={7}
+    trackColor="#F1DB4B"
+    trackSize={7}
+    onChange={(value) => { handleInputChangeIngr(value); }}
+  >
+    {/* <DragIcon x="9" y="8" width="18px" height="18px" /> */}
+  </CircularSlider>
+);
 
 
 export default Filteringredient;
-
-
--- src/components/home/searchbar/filter.js
+++ src/components/home/searchbar/filter.js
@@ -1,79 +1,73 @@
 
-import React from "react";
+import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 import Filteringredient from '../filter/filteringredient';
 import FilterDiet from '../filter/filterdiet';
 import FilterHealth from '../filter/filterhealth';
 import FilterCalories from '../filter/filtercalories';
 import FilterTime from '../filter/filtertime';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
-import '../filter/filterdiet.css'
+import '../filter/filterdiet.css';
 
 
-function Filter({handleInputChangeIngr,diet,handleChangeDiet,
-vega,vege,peanut,treenutfree,sugar,alcool,handleInputVega,handleInputVege,handleInputPeanut,handleInputNutFree,handleInputSugar,handleInputAlcool,
-handleOnChangeCalories,handleOnChangeTime,show,handleClose}) {
-   
-    return (
-      <>
-        {show
+function Filter({
+  handleInputChangeIngr, diet, handleChangeDiet,
+  vega, vege, peanut, treenutfree, sugar, alcool, handleInputVega, handleInputVege, handleInputPeanut, handleInputNutFree, handleInputSugar, handleInputAlcool,
+  handleOnChangeCalories, handleOnChangeTime, show, handleClose,
+}) {
+  return (
+    <>
+      {show
           && (
             <Modal show={show}>
               <Modal.Header>
-                <Modal.Title>Choose yours filters</Modal.Title>
-                <Button variant="secondary" onClick={handleClose}>
-                  X
-                </Button>
+                <Modal.Title>Filter selection</Modal.Title>
               </Modal.Header>
               <Modal.Body>
                 <FilterDiet
-                        diet={diet}
-                        handleChangeDiet={handleChangeDiet}
-                      />
+                  diet={diet}
+                  handleChangeDiet={handleChangeDiet}
+                />
                 <FilterHealth
-                        vega={vega}
-                        vege={vege}
-                        peanut={peanut}
-                        treenutfree={treenutfree}
-                        sugar={sugar}
-                        alcool={alcool}
-                        handleInputVega={handleInputVega}
-                        handleInputVege={handleInputVege}
-                        handleInputPeanut={handleInputPeanut}
-                        handleInputSugar={handleInputSugar}
-                        handleInputAlcool={handleInputAlcool}
-                        handleInputNutFree={handleInputNutFree}
-                        /> 
-           
-          
-                <Filteringredient 
-                        handleInputChangeIngr={handleInputChangeIngr}
-                      />
+                  vega={vega}
+                  vege={vege}
+                  peanut={peanut}
+                  treenutfree={treenutfree}
+                  sugar={sugar}
+                  alcool={alcool}
+                  handleInputVega={handleInputVega}
+                  handleInputVege={handleInputVege}
+                  handleInputPeanut={handleInputPeanut}
+                  handleInputSugar={handleInputSugar}
+                  handleInputAlcool={handleInputAlcool}
+                  handleInputNutFree={handleInputNutFree}
+                />
+
+
+                <Filteringredient
+                  handleInputChangeIngr={handleInputChangeIngr}
+                />
                 <FilterCalories
-                       handleOnChangeCalories={handleOnChangeCalories}
-                       /> 
+                  handleOnChangeCalories={handleOnChangeCalories}
+                />
 
                 <FilterTime
-                       handleOnChangeTime={handleOnChangeTime}
-                       />                           
+                  handleOnChangeTime={handleOnChangeTime}
+                />
 
               </Modal.Body>
               <Modal.Footer>
                 <Button variant="secondary" onClick={handleClose}>
-                  Close
+                  Confirm filters
                 </Button>
               </Modal.Footer>
             </Modal>
           )}
-          
-
-          
-    
-      </>
-    );
-  }
-  
- 
 
 
-  export default Filter
+    </>
+  );
+}
+
+
+export default Filter;
