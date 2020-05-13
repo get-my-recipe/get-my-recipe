0bae7a62c8d826decf8e772e16f55c968222b713
Numissa11 <aline_sinclaire@hotmail.fr>
2020-05-13 13:18:15 +0100

    verso js added code up to date

--- src/components/home/singlecard/singlecardverso.js
+++ src/components/home/singlecard/singlecardverso.js
@@ -1,52 +1,52 @@
-
+ 
 import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import Card from 'react-bootstrap/Card';
 import { ListGroup, ListGroupItem, Badge } from 'react-bootstrap';
 import './singlecard.css';
-
-
+ 
+ 
 const SingleCardVerso = ({
-  uri, title, flip, ask, ingredientLines, url,
+ uri, title, flip, ask, ingredientLines, url,
 }) => (
-  <Card onClick={() => flip(uri)}>
-    <Card.Body className="card-verso-body">
-      <Card.Link title="Link to instructions" href={url} target="_blank">
-        <Card.Title className="card-verso-title">{title}</Card.Title>
-        <hr />
-      </Card.Link>
-      {(ask === undefined)
-        ? (
-          <ul className="card-verso-ul">
-            {ingredientLines.map((el) => (
-              <li key={uuidv4()}>
-                {el}
-              </li>
-            ))}
-          </ul>
-        )
-
-        : (
-
-          <ul className="card-verso-ul">
-            {ask.map((el) => (
-              <li className={(el.ask === true) ? 'ask' : ''} key={uuidv4()}>
-                {el.ingr}
-              </li>
-            ))}
-          </ul>
-        ) }
-
-    </Card.Body>
-    <ListGroup className="list-group-flush">
-      <ListGroupItem>
-        <Badge className="badge-filter">
-          {ingredientLines.length}
-          {' '}
-          ingredients
-        </Badge>
-      </ListGroupItem>
-    </ListGroup>
-  </Card>
+ <Card onClick={() => flip(uri)}>
+   <Card.Body className="card-verso-body">
+     <Card.Link title="Link to instructions" href={url} target="_blank">
+       <Card.Title className="card-verso-title">{title}</Card.Title>
+       <hr />
+     </Card.Link>
+     {(ask === undefined)
+       ? (
+         <ul className="card-verso-ul">
+           {ingredientLines.map((el) => (
+             <li key={uuidv4()}>
+               {el}
+             </li>
+           ))}
+         </ul>
+       )
+ 
+       : (
+ 
+         <ul className="card-verso-ul">
+           {ask.map((el) => (
+             <li className={(el.ask === true) ? 'ask' : ''} key={uuidv4()}>
+               {el.ingr}
+             </li>
+           ))}
+         </ul>
+       ) }
+ 
+   </Card.Body>
+   <ListGroup className="list-group-flush">
+     <ListGroupItem>
+       <Badge className="badge-filter">
+         {ingredientLines.length}
+         {' '}
+         ingredients
+       </Badge>
+     </ListGroupItem>
+   </ListGroup>
+ </Card>
 );
 export default SingleCardVerso;
