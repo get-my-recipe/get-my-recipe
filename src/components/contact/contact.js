933b0a6aa03f75e733a268b3a86d232a75b9ab2b
Arnaud Duhé <anro010@hotmail.com>
2020-05-14 21:00:35 +0100

    added Form tag around contact form

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -25,47 +25,45 @@ class ContactForm extends Component {
   render() {
     const { message } = this.state;
     return (
-      <div className="contact-us-page">
+      <div>
         <NavBarShared />
-        <div className="containerForm">
-          <form className="contactUsForm">
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label className="label2">Name</Form.Label>
-              <Form.Control type="email" placeholder="Enter Name..." />
-            </Form.Group>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label className="label2">E-mail address</Form.Label>
-              <Form.Control
-                className="ligne"
-                type="email"
-                placeholder="Enter email..."
-              />
-              <Form.Text className="text-muted">
-                We&apos;ll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
-
-            <Form.Label className="label2">Tell us your inspiration of the day:</Form.Label>
-            <textarea
-              placeholder="Write something here..."
-              id="message-id"
-              name="message"
-              type="text"
-              value={message}
-              onChange={(event) => this.handleChange(event)}
-              rows={6}
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label className="label2">Name</Form.Label>
+            <Form.Control type="name" placeholder="Enter Name..." />
+          </Form.Group>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label className="label2">E-mail address</Form.Label>
+            <Form.Control
+              className="ligne"
+              type="email"
+              placeholder="Enter email..."
             />
-            <div className="buttonBox">
-              <button
-                type="submit"
-                className="filter-button2"
-                onClick={(event) => this.onSubmit(event)}
-              >
-                Send
-              </button>
-            </div>
-          </form>
-        </div>
+            <Form.Text className="text-muted">
+              We&apos;ll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Label className="label2">Tell us your inspiration of the day:</Form.Label>
+          <textarea
+            placeholder="Write something here..."
+            id="message-id"
+            name="message"
+            type="text"
+            value={message}
+            onChange={(event) => this.handleChange(event)}
+            rows={6}
+          />
+          <div className="buttonBox">
+            <button
+              type="submit"
+              className="filter-button2"
+              onClick={(event) => this.onSubmit(event)}
+            >
+              Send
+            </button>
+          </div>
+        </Form>
       </div>
     );
   }
